097b327a5a59fa0a34da0e350f2d26971f3b08cc
redssy <redlemagic@hotmail.com>
2024-12-06 10:02:53 -0300

    adicionando token

--- src/app/api/webhook.ts
+++ src/app/api/webhook.ts
@@ -3,18 +3,27 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 // exporta manipulador
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  // verifica tipo de requisicao: POST
-  if (req.method === 'POST') {
-    // acoes p/ posts
-    console.log('Dados recebidos do Webhook:', req.body);
-    res.status(200).json({ message: 'Webhook recebido com sucesso!' });
-  } else if (req.method === 'GET') {
-    // GET
-    console.log('Consultando dados do Webhook');
-    res.status(200).json({ message: 'Webhook recebido com sucesso!' });
+  // cria credenciais
+  const token = req.headers['x-vercel-token'];
+  const tokenEsperado = 'secreto';
+  // valida os tokens
+  if (token === tokenEsperado) {
+     // verifica tipo de requisicao: POST
+    if (req.method === 'POST') {
+      // acoes p/ posts
+      console.log('Dados recebidos do Webhook:', req.body);
+      res.status(200).json({ message: 'Webhook recebido com sucesso!' });
+    } else if (req.method === 'GET') {
+      // GET
+      console.log('Consultando dados do Webhook');
+      res.status(200).json({ message: 'Webhook recebido com sucesso!' });
+    } else {
+      // erro
+      res.status(405).json({ message: 'Método não permitido' });
+    }
   } else {
-    // erro
-    res.status(405).json({ message: 'Método não permitido' });
+    res.status(401).json({ error: 'Não autorizado.' });
+    return;
   }
 }
 
