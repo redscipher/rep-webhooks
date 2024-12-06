cddcc49aac93a41c21ace5c4645590ddf885bbdc
redssy <redlemagic@hotmail.com>
2024-12-06 13:28:06 -0300

    subindo webhook

--- api/webhook.ts
+++ api/webhook.ts
@@ -11,12 +11,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
      // verifica tipo de requisicao: POST
     if (req.method === 'POST') {
       // acoes p/ posts
-      console.log('Dados recebidos do Webhook POST:');
-      res.status(200).json({ message: 'Webhook recebido com sucesso!' });
+      console.log('Dados recebidos do Webhook POST:', req.body);
+      res.status(200).json({ message: `Webhook recebido com sucesso! ${req.body}` });
     } else if (req.method === 'GET') {
       // GET
-      console.log('Consultando dados do Webhook GET:');
-      res.status(200).json({ message: 'Webhook recebido com sucesso!' });
+      console.log('Consultando dados do Webhook GET:', req.query);
+      res.status(200).json({ message: `Webhook recebido com sucesso! ${req.query}` });
     } else {
       // erro
       res.status(405).json({ message: 'Método não permitido' });
