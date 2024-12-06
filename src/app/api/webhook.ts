35c1f28c8f4031218404d8906f4d8b759c789095
redssy <redlemagic@hotmail.com>
2024-12-06 09:41:48 -0300

    alterando pasta webhook

--- src/app/api/webhook.ts
+++ src/app/api/webhook.ts
@@ -2,18 +2,21 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 // exporta manipulador
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  // validacoes
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  // verifica tipo de requisicao: POST
   if (req.method === 'POST') {
     // acoes p/ posts
     console.log('Dados recebidos do Webhook:', req.body);
     res.status(200).json({ message: 'Webhook recebido com sucesso!' });
   } else if (req.method === 'GET') {
-    // acoes p/ gets
+    // GET
     console.log('Consultando dados do Webhook');
     res.status(200).json({ message: 'Webhook recebido com sucesso!' });
   } else {
-    // retorno de erro
+    // erro
     res.status(405).json({ message: 'Método não permitido' });
   }
 }
+
+// exportacao
+export default handler
