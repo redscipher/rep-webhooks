ac4e13c1bf914a15f6d3c5e5e61b5215331deaff
redssy <redlemagic@hotmail.com>
2024-12-06 10:54:51 -0300

    subindo paginas

--- src/app/pages/api/routes.ts
+++ src/app/pages/api/routes.ts
@@ -12,11 +12,11 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
      // verifica tipo de requisicao: POST
     if (req.method === 'POST') {
       // acoes p/ posts
-      console.log('Dados recebidos do Webhook:', req.body);
+      console.log('Dados recebidos do Webhook POST:');
       res.status(200).json({ message: 'Webhook recebido com sucesso!' });
     } else if (req.method === 'GET') {
       // GET
-      console.log('Consultando dados do Webhook:', req.query);
+      console.log('Consultando dados do Webhook GET:');
       res.status(200).json({ message: 'Webhook recebido com sucesso!' });
     } else {
       // erro
